f1f7a1af8163a009c9c8fe29b870152ca1c948e5
twalsh013 <taylor.walsh.2008@gmail.com>
2014-05-09 19:14:07 -0400

    Update test.js

--- test.js
+++ test.js
@@ -2,9 +2,23 @@ var count = parseInt(localStorage.getItem('count')) || 0;
 
 simply.on('singleClick', function(e) {
   if (e.button === 'up') {
-    simply.subtitle('wryyyy');
+    ++count;
   } else if (e.button === 'down') {
-    simply.subtitle('fuck');
+    --count;
+  }
+  if (count > 3){
+    count = 0;
+  } else if (count < 0){
+    count = 3;
+  } 
+  if (count === 3 ){
+    simply.subtitle('brightness');
+  } else if (count === 2 ){
+    simply.subtitle('blue');
+  } else if (count === 1 ){
+    simply.subtitle('green');
+  } else if (count === 0 ){
+    simply.subtitle('red');
   }
   localStorage.setItem('count', count);
 });
